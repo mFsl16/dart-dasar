66ccffbacfc3fa18cd3006da4f0b090e9df83e47
MFaisal <MFaisal>
2022-05-01 10:36:55 +0700

    initial commit

---
+++ basic_dart_program.dart
@@ -0,0 +1,9 @@
+// Define a function
+void printInteger(int aNumber) {
+  print('The number is $aNumber');
+}
+
+void main(List<String> args) {
+  var number = 42;
+  printInteger(number);
+}
---
+++ variable.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+
+  // initializing variable
+  var name = 'Bob';
+
+  // spesifying variable type
+  Object specificName = 'Jhon';
+
+  // explicit declaring name as string
+  String stringName = 'Robert';
+
+  // DEFAULT VALUE
+
+  // nullable variable
+  int? lineCount;
+  assert(lineCount == null);
+
+  // null safety variable
+  int safeLineCount = 0;
+
+
+}
